a008d012646e62e8f3746bd53d4210b90561d2f2
Patrick Thomson <pthomson@apple.com>
2012-04-23 20:12:37 -0400

    get some readline love

--- src/Main.hs
+++ src/Main.hs
@@ -4,24 +4,27 @@ module Main where
   import Assembler
   import Parser
   import Pretty
+  import Data.Maybe
+  import System.Console.Readline
   import Text.Parsec (parseTest)
   
   main :: IO ()
   main = do
-    putStrLn "hs-dcpu started in interactive mode. Ctrl-C exits."
+    putStrLn "hs-dcpu started in interactive mode. Ctrl-D exits."
     inputLoop
     where
       inputLoop :: IO ()
       inputLoop = do
-        putStr ">>> "
-        prog <- getLine
-        let e = parse program "" (pack prog)
-        case e of
-          (Left err) -> print err
-          (Right pro) -> do
-            print pro
-            let asm = doAssembly pro
-            print $ pretty <$> asm
-            
-        inputLoop
+        p <- readline ">>> "
+        case p of
+          Nothing -> return ()
+          Just prog -> do
+            let e = parse program "" (pack prog)
+            case e of
+              (Left err) -> print err
+              (Right pro) -> do
+                print pro
+                let asm = doAssembly pro
+                print $ pretty <$> asm
+            inputLoop
   
